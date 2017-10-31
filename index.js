9b5c47c19f2d3cf7579ba43a4dba725ec5be5acd
Luke Conley <luke.sovern.conley@gmail.com>
2017-10-31 00:33:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // write your code below!
-function happyHolidays () {
+function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
+}
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
 }
